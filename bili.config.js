af0157fa6d6346cb1c278f1d3b395fc55e207f8e
unknown <yanlibo2010@163.com>
2020-01-18 23:09:21 +0800

    add src

--- bili.config.js
+++ bili.config.js
@@ -1,11 +1,27 @@
+import { uglify } from "rollup-plugin-uglify";
+// const uglify =require("rollup-plugin-uglify");
 module.exports = {
   banner: true,
   output: {
-    extractCSS: false
+    dir: "lib"
   },
   plugins: {
     vue: {
       css: true
     }
   }
+  // extractCSS: true,
+  // minify: true,
+  // RollupConfig: {
+  //   outputConfig: {},
+  //   plugins: [uglify()]
+  // }
+  // plugins: [
+  //   {
+  //     vue: {
+  //       css: true
+  //     }
+  //   },
+  //   uglify()
+  // ]
 };
