6ba92054a95082ae41dad1ffec948bba927e3ae0
yanlibo2013 <yanlibo2010@163.com>
2020-01-19 19:20:42 +0800

    modified:   package-lock.json         modified:   package.json         modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ export default {
   install(Vue, options) {
     // Let's register our component globally
     // https://vuejs.org/v2/guide/components-registration.html
-    Vue.component("jsplumb-chart", jsplumbchart);
+    Vue.component("jsplumbchart", jsplumbchart);
   }
 };
